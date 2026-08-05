9ecec3ae24917d244f6de8a9a495b0163d2eea93
Кирилл Вешняков <kirillveshnakov@gmail.com>
2026-08-05 15:54:39 +0300

    add project module entity

---
+++ backend/internal/project/entity.go
@@ -0,0 +1,25 @@
+package project
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Element struct {
+	ID          uuid.UUID
+	ProjectID   uuid.UUID
+	Key         string
+	Label       string
+	Description string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+type Project struct {
+	ID         uuid.UUID
+	Name       string
+	ProjectKey string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
