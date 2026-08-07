1143cfa331cc01b8a1341e553ddfa8329fec2ca5
Кирилл Вешняков <kirillveshnakov@gmail.com>
2026-08-07 17:42:38 +0300

    add scenario entity and errs

---
+++ backend/internal/scenario/errs/validations.go
@@ -0,0 +1,20 @@
+package errs
+
+import "errors"
+
+var (
+	ErrStepElementIDRequired  = errors.New("step element id is required")
+	ErrStepScenarioIDRequired = errors.New("step scenario id is required")
+	ErrStepTitleRequired      = errors.New("step title is required")
+	ErrInvalidStepNumber      = errors.New("step number is invalid")
+	ErrStepTitleTooLong       = errors.New("step title is too long")
+	ErrStepDescriptionTooLong = errors.New("step description is too long")
+
+	ErrScenarioProjectIDRequired   = errors.New("scenario project id is required")
+	ErrScenarioNameRequired        = errors.New("scenario name is required")
+	ErrScenarioPagePatternRequired = errors.New("scenario page pattern is required")
+	ErrScenarioNameTooLong         = errors.New("scenario name is too long")
+	ErrScenarioDescriptionTooLong  = errors.New("scenario description is too long")
+	ErrScenarioPagePatternTooLong  = errors.New("scenario page pattern is too long")
+	ErrScenarioStatusUnknown       = errors.New("scenario status unknown")
+)
