6782359f161ee18c35c672598adfe7861a5ff801
Кирилл Вешняков <kirillveshnakov@gmail.com>
2026-08-07 04:47:06 +0300

    change config struct

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -18,27 +18,31 @@ type (
 		IdleTimeout       time.Duration `env:"HTTP_IDLE_TIMEOUT" envDefault:"60s"`
 		HTTPShutdownTime  time.Duration `env:"HTTP_SHUTDOWN_TIME" envDefault:"30s"`
 	}
+
+	PGConfig struct {
+		Host     string `env:"POSTGRES_HOST" envDefault:"localhost"`
+		Port     string `env:"POSTGRES_PORT" envDefault:"5432"`
+		DB       string `env:"POSTGRES_DB" envDefault:"onboarding"`
+		User     string `env:"POSTGRES_USER" envDefault:"onboarding_user"`
+		Password string `env:"POSTGRES_PASSWORD" envDefault:"12345"`
+	}
+
 	Config struct {
-		PG struct {
-			Host     string `env:"POSTGRES_HOST" envDefault:"localhost"`
-			Port     string `env:"POSTGRES_PORT" envDefault:"5432"`
-			DB       string `env:"POSTGRES_DB" envDefault:"onboarding"`
-			User     string `env:"POSTGRES_USER" envDefault:"onboarding_user"`
-			Password string `env:"POSTGRES_PASSWORD" envDefault:"12345"`
-		}
+		HTTPConfig HTTPConfig
+		PGConfig   PGConfig
 	}
 )
 
-func (c HTTPConfig) Address() string {
+func (c *HTTPConfig) HTTPAddress() string {
 	return net.JoinHostPort(c.Host, c.Port)
 }
 
 func (c *Config) ConstructPostgresURL() string {
 	return fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable",
-		c.PG.User,
-		c.PG.Password,
-		net.JoinHostPort(c.PG.Host, c.PG.Port),
-		c.PG.DB,
+		c.PGConfig.User,
+		c.PGConfig.Password,
+		net.JoinHostPort(c.PGConfig.Host, c.PGConfig.Port),
+		c.PGConfig.DB,
 	)
 }
 
