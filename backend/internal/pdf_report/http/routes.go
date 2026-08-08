188877c54096069920fb9f536930b030fe6b706d
Daniil Sintsov <daniilsintsov@mail.ru>
2026-08-08 22:45:33 +0300

    Revert "Merge branch 'develop' of https://github.com/DaniilSintsov/interactive-onboarding into feature/frontend"
    This reverts commit 9b27c9336d6d7ddea3d318a2d321ba35aab05a0c, reversing
    changes made to 8691401cfc8b427e6e648e04efc929f1f3d28afd.

--- backend/internal/pdf_report/http/routes.go
+++
@@ -1,9 +0,0 @@
-package pdfhttp
-
-import (
-	"net/http"
-)
-
-func (h *PDFHandler) RegisterRoutes(mux *http.ServeMux) {
-	mux.HandleFunc("GET /api/v1/scenarios/{scenarioId}/report/pdf", h.GenerateScenarioPDFReport)
-}
