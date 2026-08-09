b6a0c6d42cdcfbf14d98ca880261e63c5311241a
Кирилл Вешняков <kirillveshnakov@gmail.com>
2026-08-09 17:02:33 +0300

    fix misspellÐ

--- backend/internal/platform/middleware/admintoken.go
+++ backend/internal/platform/middleware/admintoken.go
@@ -19,12 +19,12 @@ func (a *AdminMiddleware) CheckAdminToken(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
-			writeProjectKeyError(w, http.StatusUnauthorized, "unathorized", "admin token is not provided")
+			writeProjectKeyError(w, http.StatusUnauthorized, "unauthorized", "admin token is not provided")
 			return
 		}
 		parts := strings.Fields(authHeader)
 		if len(parts) != 2 || parts[0] != "Bearer" || parts[1] != a.token {
-			writeProjectKeyError(w, http.StatusUnauthorized, "unathorized", "incorrect admin token")
+			writeProjectKeyError(w, http.StatusUnauthorized, "unauthorized", "incorrect admin token")
 			return
 		}
 		next.ServeHTTP(w, r)
